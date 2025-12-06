f0496f8ac85093b6420709f17f287480e429a554
rinnli_o47 <witherlzero@outlook.com>
2025-12-06 11:02:17 +0800

    Expose Variables to Blueprints

--- Source/Slash/Public/Items/Item.h
+++ Source/Slash/Public/Items/Item.h
@@ -23,7 +23,12 @@ protected:
 	
 private:
 	float RunningTime;
+	
+	// UPROPERTY(EditDefaultsOnly)： can see and edit variables in blueprints
+	// UPROPERTY(EditInstanceOnly)： can see and edit variables on instances
+	UPROPERTY(EditAnywhere) // can see and edit variables both in blueprints and on instances
 	float Amplitude;
+	UPROPERTY(EditAnywhere)
 	float TimeConstant;
 
 };
