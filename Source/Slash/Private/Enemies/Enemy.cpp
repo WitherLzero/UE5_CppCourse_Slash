23979a9d771555ef2877033bccf6db3015bebbc9
rinnli_o47 <witherlzero@outlook.com>
2025-12-13 19:59:41 +0800

    Section Weapon Mechanic: Create and setup Enemy class

---
+++ Source/Slash/Private/Enemies/Enemy.cpp
@@ -0,0 +1,35 @@
+﻿// copyright: none
+
+
+#include "Enemies/Enemy.h"
+
+#include "Components/CapsuleComponent.h"
+
+
+AEnemy::AEnemy()
+{
+	PrimaryActorTick.bCanEverTick = true;
+	
+	GetMesh()->SetCollisionObjectType(ECollisionChannel::ECC_WorldDynamic);
+	GetMesh()->SetGenerateOverlapEvents(true);
+	GetMesh()->SetCollisionResponseToChannel(ECollisionChannel::ECC_Visibility,ECollisionResponse::ECR_Block);
+	GetMesh()->SetCollisionResponseToChannel(ECollisionChannel::ECC_Camera,ECR_Ignore);
+	GetCapsuleComponent()->SetCollisionResponseToChannel(ECC_Camera,ECR_Ignore);
+}
+
+void AEnemy::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+void AEnemy::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+}
+
+void AEnemy::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+}
+
