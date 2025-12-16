a0a74fb90b0c5fe02471063997ec69ac2505b402
rinnli_o47 <witherlzero@outlook.com>
2025-12-16 12:43:49 +0800

    Section Treasure: Create Treasure class with pickup sound and interaction logic

---
+++ Source/Slash/Private/Items/Pickups/Treasure.cpp
@@ -0,0 +1,43 @@
+﻿// copyright: none
+
+
+#include "Items/Pickups/Treasure.h"
+
+#include "Characters/SlashCharacter.h"
+#include "Kismet/GameplayStatics.h"
+
+
+// Sets default values
+ATreasure::ATreasure()
+{
+	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+}
+
+// Called when the game starts or when spawned
+void ATreasure::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+void ATreasure::OnSphereOverlap(UPrimitiveComponent* OverlappedComp, AActor* OtherActor, UPrimitiveComponent* OtherComp,
+	int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
+{
+	if (ASlashCharacter* OverlappingCharacter = Cast<ASlashCharacter>(OtherActor))
+	{
+		if (PickupSound)
+		{
+			UGameplayStatics::PlaySoundAtLocation(this, PickupSound, GetActorLocation());
+		}
+		Destroy();
+	}
+	
+}
+
+// Called every frame
+void ATreasure::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+}
+
---
+++ Source/Slash/Public/Items/Pickups/Treasure.h
@@ -0,0 +1,26 @@
+﻿// copyright: none
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Items/Item.h"
+#include "Treasure.generated.h"
+
+UCLASS()
+class SLASH_API ATreasure : public AItem
+{
+	GENERATED_BODY()
+
+public:
+	ATreasure();
+	virtual void Tick(float DeltaTime) override;
+
+protected:
+	virtual void BeginPlay() override;
+
+	virtual void OnSphereOverlap(UPrimitiveComponent* OverlappedComp, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult) override;
+
+private:
+	UPROPERTY(EditAnywhere,category = "Sounds")
+	USoundBase* PickupSound;
+};
