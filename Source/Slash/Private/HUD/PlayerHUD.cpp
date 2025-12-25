0c896e40d27b39b036b9afac9e69af9a862cedf9
rinnli_o47 <witherlzero@outlook.com>
2025-12-26 03:59:19 +0800

    Section Echo's Attributes: Create `PlayerHUD` class to handle Overlay widget initialization

---
+++ Source/Slash/Private/HUD/PlayerHUD.cpp
@@ -0,0 +1,23 @@
+﻿// copyright: none
+
+
+#include "HUD/PlayerHUD.h"
+
+#include "Blueprint/UserWidget.h"
+#include "HUD/PlayerOverlay.h"
+
+void APlayerHUD::BeginPlay()
+{
+	Super::BeginPlay();
+
+	if (UWorld* World = GetWorld())
+	{
+		APlayerController* Controller = World->GetFirstPlayerController();
+		if (Controller && OverlayClass)
+		{
+			PlayerOverlay = CreateWidget<UPlayerOverlay>(Controller,OverlayClass);
+			PlayerOverlay->AddToViewport();
+		}
+	}
+	
+}
---
+++ Source/Slash/Public/HUD/PlayerHUD.h
@@ -0,0 +1,27 @@
+﻿// copyright: none
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/HUD.h"
+#include "PlayerHUD.generated.h"
+
+class UPlayerOverlay;
+/**
+ * 
+ */
+UCLASS()
+class SLASH_API APlayerHUD : public AHUD
+{
+	GENERATED_BODY()
+	
+protected:
+	virtual void BeginPlay() override;
+	
+private:
+	UPROPERTY(EditDefaultsOnly,Category = HUD)
+	TSubclassOf<UPlayerOverlay> OverlayClass;
+	
+	UPROPERTY()
+	UPlayerOverlay* PlayerOverlay;
+};
